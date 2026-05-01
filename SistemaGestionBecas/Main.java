b7164d34e0cf3bbba6d289ad6f93af2c66c5eac5
AndersonEstrada-cisco <a.estradawml5@gmail.com>
2026-05-01 06:54:16 +0100

    Crear objetos y solicitar datos por teclado

--- SistemaGestionBecas/Main.java
+++ SistemaGestionBecas/Main.java
@@ -1,13 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main{
+        public static void main(String[] args){
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print(" \nCodigo: ");
+            String codigo = sc.nextLine();
+
+            System.out.print(" Estudiante: ");
+            String nombre = sc.nextLine();
+
+            System.out.print("Promedio: ");
+            double promedio = sc.nextDouble();
+
+            System.out.print("Valor de matrícula: ");
+            double valorMatricula = sc.nextDouble();
+
+            System.out.print("Número de materias: ");
+            int numeroMaterias = sc.nextInt();
+
+            System.out.print("Recargo pendiente: ");
+            double recargo = sc.nextDouble();
+
+            System.out.print("Porcentaje de beca: ");
+            int porcentaje = sc.nextInt();
+
+            System.out.print("Bono excelencia: ");
+            double bonoExcelencia = sc.nextDouble();
+
+            /* EstudianteRegular e1 = new EstudianteRegular(codigo, nombre, promedio, valorMatricula, numeroMaterias);
+            e1.mostrarDatos();
+
+            EstudianteBecado e2 = new EstudianteBecado(codigo, nombre, promedio, valorMatricula, porcentaje);
+            e2.mostrarDatos();
+
+            RegularConRecargo e3 = new RegularConRecargo(codigo, nombre, promedio, valorMatricula, numeroMaterias, recargo);
+            e3.mostrarDatos();
+        */
+
+            BecadoExcelencia e4 = new BecadoExcelencia(codigo, nombre, promedio, valorMatricula, porcentaje, bonoExcelencia);
+            e4.mostrarDatos();
+
+        }
     }
-}
