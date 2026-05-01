52a7be6c70ef0265f9725d8cd88749b715d4c3f1
AndersonEstrada-cisco <a.estradawml5@gmail.com>
2026-05-01 20:14:23 +0100

    Update Main.java

--- SistemaColaboradorAcademico/Main.java
+++ SistemaColaboradorAcademico/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print(" Código: ");
+        String codigo = sc.nextLine();
+
+        System.out.print(" Nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print(" Edad: ");
+        int edad = sc.nextInt();
+
+        System.out.print(" Horas de clase: ");
+        int horas = sc.nextInt();
+
+        System.out.print(" Valor por hora: ");
+        double valor = sc.nextDouble();
+
+        System.out.print(" Publicaciones: ");
+        int publicaciones = sc.nextInt();
+
+        PersonaAcademica docenteInvestigador = new DocenteInvestigador(codigo, nombre, edad, horas, valor, publicaciones);
+        docenteInvestigador.mostrarDatos();
+
+        docenteInvestigador.describirRol();
+        System.out.println(" Pago final: $" + docenteInvestigador.calcularPago());
     }
 }
