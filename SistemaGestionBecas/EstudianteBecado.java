85f7d23ef5770640e79c4def11c5dbcae6634669
AndersonEstrada-cisco <a.estradawml5@gmail.com>
2026-05-01 06:52:45 +0100

    Agregar funcionalidad de beca

--- SistemaGestionBecas/EstudianteBecado.java
+++ SistemaGestionBecas/EstudianteBecado.java
@@ -1,2 +1,26 @@
 public class EstudianteBecado extends Estudiante {
+  private int porcentajeBeca;
+
+    public EstudianteBecado(String codigo, String nombre, double promedio, double valorMatricula, int porcentajeBeca){
+        super(codigo, nombre, promedio, valorMatricula);
+        setPorcentajeBeca(porcentajeBeca);
+    }
+
+    public void setPorcentajeBeca(int porcentajeBeca){
+        if(porcentajeBeca>0 && porcentajeBeca<=100){
+            this.porcentajeBeca = porcentajeBeca;
+        }else {
+            System.out.println(" ** PORCENTAJE INVALIDO");
+        }
+    }
+
+    public int getPorcentajeBeca() {
+        return porcentajeBeca;
+    }
+
+    @Override
+    public double calcularPagoFinal(){
+        double pagoBeca = getValorMatricula() - (getValorMatricula()*(getPorcentajeBeca()/100.0));
+        return pagoBeca;
+    }
 }
