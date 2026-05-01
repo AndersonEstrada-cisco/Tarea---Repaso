a65fc7affcff2e62ef93a87bf4ebc554d0f4669d
AndersonEstrada-cisco <a.estradawml5@gmail.com>
2026-05-01 20:12:26 +0100

    Implementar la clase Docente con atributos y métodos

--- SistemaColaboradorAcademico/Docente.java
+++ SistemaColaboradorAcademico/Docente.java
@@ -1,2 +1,39 @@
-public class Docente {
+public class Docente extends PersonaAcademica{
+    private int horasClase;
+    private double valorHora;
+
+    public Docente(String codigo, String nombre, int edad, int horasClase, double valorHora) {
+        super(codigo, nombre, edad);
+        setHorasClase(horasClase);
+        if (valorHora > 0) {
+            this.valorHora = valorHora;
+        }
+    }
+
+    public void setHorasClase(int horas) {
+        if (horas >= 1 && horas <= 40) {
+            this.horasClase = horas;
+        }
+    }
+
+    public int getHorasClase() {
+        return horasClase;
+    }
+
+    @Override
+    public double calcularPago() {
+        return horasClase * valorHora;
+    }
+
+    @Override
+    public void mostrarDatos() {
+        super.mostrarDatos();
+        System.out.println(" Horas de clase: " + horasClase);
+        System.out.println(" Valor por hora: " + valorHora);
+    }
+
+    @Override
+    public void describirRol() {
+        System.out.println(" Rol: Docente.");
+    }
 }
