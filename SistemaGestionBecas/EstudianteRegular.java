557d19f0c9a9c3f579dd4bc72098b621ec05ecc0
AndersonEstrada-cisco <a.estradawml5@gmail.com>
2026-05-01 06:53:37 +0100

    Agregar numeroMaterias y cálculo de pago a Estudiante Regular

--- SistemaGestionBecas/EstudianteRegular.java
+++ SistemaGestionBecas/EstudianteRegular.java
@@ -1,2 +1,22 @@
 public class EstudianteRegular extends Estudiante{
+    private int numeroMaterias;
+
+    public EstudianteRegular(String codigo, String nombre, double promedio, double valorMatricula, int numeroMaterias){
+        super(codigo, nombre, promedio, valorMatricula);
+        setNumeroMaterias(numeroMaterias);
+    }
+
+    public void setNumeroMaterias(int numeroMaterias){
+        if(numeroMaterias>0){
+            this.numeroMaterias = numeroMaterias;
+        } else {
+            System.out.println("\n ** Debe haber materias ");
+        }
+    }
+
+    @Override
+    public double calcularPagoFinal(){
+        double pagoRegular = getValorMatricula() + (numeroMaterias*10);
+        return pagoRegular;
+    }
 }
